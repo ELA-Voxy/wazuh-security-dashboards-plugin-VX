34ec8c273b90f9a4b13137ee278207fb9d1d3458
ELA-VOXY <emeric.lai@voxy.fr>
2025-10-22 00:32:33 +0200

    (UP) - VX RED logo

--- public/apps/login/login-page.tsx
+++ public/apps/login/login-page.tsx
@@ -284,7 +284,7 @@ export function LoginPage(props: LoginPageDeps) {
         <EuiImage
           size="fullWidth"
           alt=""
-          url={props.config.ui.basicauth.login.brandimage || defaultBrandImage}
+          url={defaultBrandImage}
         />
       )}
       <EuiSpacer size="s" />
